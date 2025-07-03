9be6f783a9344d6de5a0d711d99556f61d123fa5
Ubuntu <ubuntu@ip-172-31-87-208.ec2.internal>
2025-07-03 06:33:24 +0000

    removed s3 backend for now

--- main.tf
+++ main.tf
@@ -7,12 +7,12 @@ terraform {
   }
   required_version = ">= 0.12"
 
-  backend "s3" {
-    bucket  = "terraform-ansible-project-bucket"
-    key     = "env/dev/terraform.tfstate"
-    region  = "ap-south-1"
-    encrypt = true
-  }
+  # backend "s3" {
+  #   bucket  = "terraform-ansible-project-bucket"
+  #   key     = "env/dev/terraform.tfstate"
+  #   region  = "ap-south-1"
+  #   encrypt = true
+  # }
 }
 
 provider "vault" {
